bef29a1a56dab4e0a91528fcd1ae1ce5b2284034
Anuj Khare <khareanuj18@gmail.com>
2016-02-05 02:39:44 +0530

    2: working

--- 2_broadcast.cpp
+++ 2_broadcast.cpp
@@ -17,11 +17,10 @@ int main(int argc, char **argv)
 	MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 	
-	int part_size = ARR_SIZE / num_procs;
+	int part_size = int(ARR_SIZE / num_procs);
 
 	/* Initialize the array */
 	int arr[ARR_SIZE];
-	//printf("Hey there %d\n", myrank);
 
 	if(0 == myrank) {
 		for(int i=0; i<ARR_SIZE; ++i) {
@@ -29,30 +28,31 @@ int main(int argc, char **argv)
 		}
 	}
 
-	if (0 == myrank) {
-		printf("0000 %d %d\n", arr[0], arr[ARR_SIZE-1]);
-		/* Broadcast to each process */
-		MPI_Bcast(arr, ARR_SIZE, MPI_INT, 0, MPI_COMM_WORLD);
-		printf("Broadcast array\n");
-	} else {
-		printf("Received %d %d\n", arr[0], arr[ARR_SIZE-1]);
-	}
+	/* Broadcast to each process */
+	MPI_Bcast(arr, ARR_SIZE, MPI_INT, 0, MPI_COMM_WORLD);
 
-	long long sum = 0;
+	//if (1==myrank) {
+	//	for(int i=0; i<ARR_SIZE; ++i) {
+	//		cout<<"Printing arr "<<arr[i]<<endl;
+	//	}
+	//}
 
 	/* sum up your own part */
+	long long sum = 0;
 	for(int i=myrank*part_size; i<(myrank+1)*part_size; ++i) {
 		sum += arr[i];
 	}
 
 	if (0 != myrank) {
 		/* send the sum back */
-		MPI_Send(&sum, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+		MPI_Send(&sum, 1, MPI_LONG, 0, 0, MPI_COMM_WORLD);
+
 	} else {
 		/* receive from others and sum up */
 		long long temp = 0;
 		for (int p=1; p<num_procs; ++p) {
-			MPI_Recv(&temp, 1, MPI_INT, p, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(&temp, 1, MPI_LONG, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			//printf("Received from %d: %d\n", p, temp);
 			sum += temp;
 		}
 		printf("The final sum is %d\n", sum);
